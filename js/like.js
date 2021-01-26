90186c5bb2afdda9fcdc8ac02e381185f2565d1f
William Teles <teles967@gmail.com>
2021-01-26 20:04:32 -0300

    Arrumando função de like para os posts dinamicos

--- js/like.js
+++ js/like.js
@@ -1,19 +1,18 @@
 let flag = 0;
-		let qutLikes = parseInt(document.getElementById('likesCount').innerText);
-		console.log(qutLikes);
-		
 
-		function iLike() {
-			if (flag === 0){
-				document.getElementById('likes').style.fill = '#17bf63';
-				qutLikes = qutLikes + 1;
-				document.getElementById('likesCount').innerHTML = qutLikes;
-				flag = 1;
-			} else {
-				document.getElementById('likes').style.fill = '#18191a';
-				qutLikes = qutLikes - 1;
-				document.getElementById('likesCount').innerHTML = qutLikes;
-				flag = 0;
-			}
+function iLike(x) {
+	let y = String(x.id).split("likes").pop()
+	let qutLikes = parseInt(document.getElementById('likesCount' + y).innerText);
+	if (flag === 0) {
+		document.getElementById('likes' + y).style.fill = '#17bf63';
+		qutLikes = qutLikes + 1;
+		document.getElementById('likesCount' + y).innerText = qutLikes;
+		flag = 1;
+	} else {
+		document.getElementById('likes' + y).style.fill = '#18191a';
+		qutLikes = qutLikes - 1;
+		document.getElementById('likesCount' + y).innerText = qutLikes;
+		flag = 0;
+	}
 
-		}
+}
