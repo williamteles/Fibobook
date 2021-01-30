3bd5b10ad0276d1824fda8aa5bc6d4cbef3c1d4e
Alex Emanuel <alexprojetos27@gmail.com>
2021-01-30 19:00:43 -0300

    Fix: Tamanho da imagem na preview do post

--- js/upload-image.js
+++ js/upload-image.js
@@ -14,8 +14,7 @@ function handleFiles() {
     let img = document.createElement('img')
     img.src = window.webkitURL.createObjectURL(this.files[0])
     img.height = 200
-    img.setAttribute('style', 'grid-row: 3; grid-column: 3; border: 2px groove #9a9a9a')
-    img.setAttribute('id', 'uploaded-img')
+    img.id = "uploaded-img";
     img.onload = function () {
         URL.revokeObjectURL(this.src)
     }
