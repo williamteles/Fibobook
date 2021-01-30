a7e5e482e8b42cb015ecf65bc29a8d5bcff5b7c3
Alex Emanuel <alexprojetos27@gmail.com>
2021-01-30 18:37:27 -0300

    Função para criação de novos posts do usuário

---
+++ js/create-new-user-post.js
@@ -0,0 +1,39 @@
+import generatePostNode from "./generate-post-node.js"
+import User from "./user.js";
+
+
+function createNewUserPost() {  
+    const feedNode = document.querySelector("#feed");
+    const postDescriptionNode = document.getElementById("create-description");
+    const postImageNode = document.querySelector("#uploaded-img");
+    const emojiAreaNode = document.querySelector("#area-emojis");
+
+    let postDescription = postDescriptionNode.value;
+    let postImageSrc = postImageNode ? postImageNode.getAttribute("src") : null;
+
+    if(postDescription == "" && postImageSrc == null) {
+        alert("Preencha o campo de texto para poder postar.");
+    }
+    
+    let likeNumber = 0;
+
+    let username = "Erick Jacquin";
+    let userProfileImageURL = "image/profile-photos/pp-Jacquin.jpg";
+    let user = new User(username, userProfileImageURL);
+
+    const postNode = generatePostNode(user, postDescription, likeNumber, postImageSrc);
+    feedNode.prepend(postNode);
+
+    // Reset the new posts creation section
+    postDescriptionNode.value = "";
+
+    if(postImageNode) {
+        postImageNode.remove();
+    }
+    if(emojiAreaNode) {
+        emojiAreaNode.remove();
+    }
+}
+
+const postButton = document.querySelector("#post-button");
+postButton.addEventListener("click", createNewUserPost);    
