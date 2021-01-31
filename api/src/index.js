850559ec82220a893a916f310a8e82c783721efa
Alex Emanuel <alexprojetos27@gmail.com>
2021-01-31 15:58:02 -0300

    Posts do feed são carregados da api

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,8 @@
 const Hapi = require("@hapi/hapi");
+
 const postsUpdateFeed = require("../data/posts_update_feed");
 const postsProfile = require("../data/posts_profile");
+const postsFeed = require("../data/posts_feed");
 
 
 const init = async () => {
@@ -25,6 +27,17 @@ const init = async () => {
 		path: "/api/posts_profile",
 		handler: function(req, res) {
 			return postsProfile;
+		},
+		options: {
+			cors: true
+		}
+	});
+
+	server.route({
+		method: "GET",
+		path: "/api/posts_feed",
+		handler: function(req, res) {
+			return postsFeed;
 		},
 		options: {
 			cors: true
