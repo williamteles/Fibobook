9297e21fc3c28b389a10bf97f45f5cb9875dda09
Alex Emanuel <alexprojetos27@gmail.com>
2021-01-31 14:57:47 -0300

    Novo endpoint para carregar os posts do perfil

---
+++ api/data/posts_profile.js
@@ -0,0 +1,48 @@
+module.exports = [
+	{
+		"user": {
+			"name": "Erick Jacquin",
+			"profileImageUrl": "image/profile-photos/pp-Jacquin.jpg",
+		},
+		"description": "Eu e meu peru gordo?",
+	 	"imageUrl": "https://www.portalsabores.com.br/wp-content/uploads/2016/11/Jacquin.jpg",
+		"likeNumber": "20",
+	},
+
+	{
+		"user": {
+			"name": "Erick Jacquin",
+			"profileImageUrl": "image/profile-photos/pp-Jacquin.jpg",
+		},
+		"description": "Que banana gostosa",
+	 	"imageUrl": "https://f.i.uol.com.br/fotografia/2019/06/06/15598529795cf977b3e9c6c_1559852979_1x1_md.jpg",
+		"likeNumber": "2",
+	},
+	{
+		"user": {
+			"name": "Erick Jacquin",
+			"profileImageUrl": "image/profile-photos/pp-Jacquin.jpg",
+		},
+		"description": "E que venha o carnaval de 2021",
+	 	"imageUrl": "https://viroupauta.com/wp-content/uploads/2020/12/Erick-Jacquin-faz-homenagem-a-mandioca.jpg",
+		"likeNumber": "5",
+	},
+	{
+		"user": {
+			"name": "Erick Jacquin",
+			"profileImageUrl": "image/profile-photos/pp-Jacquin.jpg",
+		},
+		"description": "Ovo mata o batima 🤡",
+	 	"imageUrl": "https://pbs.twimg.com/media/EHgX6zZXUAEtijc.jpg",
+		"likeNumber": "400",
+	},
+	{
+		"user": {
+			"name": "Erick Jacquin",
+			"profileImageUrl": "image/profile-photos/pp-Jacquin.jpg",
+		},
+		"description": "Eu sou uma piada pra você?",
+			"imageUrl": "https://images7.memedroid.com/images/UPLOADED933/5dcde583960ee.jpeg",
+			"likeNumber": "5",
+	},
+]
--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 const Hapi = require("@hapi/hapi");
-const posts = require("../data/posts");
+const postsUpdateFeed = require("../data/posts_update_feed");
+const postsProfile = require("../data/posts_profile");
 
-let cont = 0;
 
 const init = async () => {
 	const server = Hapi.server({
@@ -11,12 +11,23 @@ const init = async () => {
 	
 	server.route({
 		method: "GET",
-		path: "/api/posts",
+		path: "/api/posts_update_feed",
 		handler: function(req, res) {
-			return posts;
+			return postsUpdateFeed;
 		},
 		options: {
     		cors: true
+		}
+	});
+
+	server.route({
+		method: "GET",
+		path: "/api/posts_profile",
+		handler: function(req, res) {
+			return postsProfile;
+		},
+		options: {
+			cors: true
 		}
 	});
 
