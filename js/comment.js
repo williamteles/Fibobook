fb67cba9506050ece1eeae5cf68500d2dbc1d29c
Gabriel <gabriellima.glgs@gmail.com>
2021-02-04 12:57:38 -0300

    minor fixes

--- js/comment.js
+++ js/comment.js
@@ -42,11 +42,11 @@ function addComment(buttonComment) {
         SVGPath1.setAttribute('d','M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z');
         SVGPath1.setAttribute('fill', 'none');
 
-        const SVGPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-        SVGPath2.setAttribute('d', 'M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z')
+        const SVGPath2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        SVGPath2.setAttribute('d', 'M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z');
         SVGPath2.setAttribute('fill-rule', 'evenodd');
         
-        deleteCommentNode.appendChild(SVGPath2)
+        deleteCommentNode.appendChild(SVGPath2);
         commentNode.appendChild(deleteCommentNode);
 
         // Comment
@@ -64,5 +64,8 @@ function addComment(buttonComment) {
 }
 
 function deleteComment(comment) {
-    comment.parentNode.remove()
+    let deleteCommentText = confirm('Tem certeza?');
+    if (deleteCommentText){
+        comment.parentNode.remove()
+    }
 }   
