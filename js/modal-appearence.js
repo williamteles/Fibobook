235bf81f93a129ae50e360d4c8dc5487f17336c5
Alex Emanuel <alexprojetos27@gmail.com>
2021-02-06 22:27:59 -0300

    Ocultar o form section do login quando chamar o cadastro

--- js/modal-appearence.js
+++ js/modal-appearence.js
@@ -1,6 +1,8 @@
 const singUpButton = document.getElementById("singUp-link")
 const modal = document.querySelector("#modal")
+const formSection = document.querySelector(".form-section");
 
 singUpButton.addEventListener("click", () => {
+    formSection.style.display = "none";
     modal.classList.toggle("hide")
 })
